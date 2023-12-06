bafbbca65a25dccc5f872d9f76e63cc04651f4ef
iscruc <ismaelcruc@gmail.com>
2023-12-06 17:20:35 +0100

    feat: printf, header, Makefile are created

---
+++ printf.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   printf.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: icruces- < icruces-@student.42malaga.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/06 17:06:02 by icruces-          #+#    #+#             */
+/*   Updated: 2023/12/06 17:07:44 by icruces-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef libftprintf
+# define libftprintf
+# include <stdio.h>
+# include <stdarg.h>
+# include <stdlib.h>
+# include <string.h>
+
+	int ft_printf(char const *, ...)
+	
+#endif
+
+
