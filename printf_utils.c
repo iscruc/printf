f9cfb719fbc102232d785dd86bd00e3db560d5c7
iscruc <ismaelcruc@gmail.com>
2024-01-02 19:20:41 +0100

    added putnbrbase

--- printf_utils.c
+++ printf_utils.c
@@ -6,11 +6,21 @@
 /*   By: icruces- < icruces-@student.42malaga.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/07 16:49:05 by icruces-          #+#    #+#             */
-/*   Updated: 2023/12/07 20:54:22 by icruces-         ###   ########.fr       */
+/*   Updated: 2024/01/02 19:16:06 by icruces-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "printf.h"
+
+static int	ft_strlen(char const *str)
+{
+	int	len;
+
+	len = 0;
+	while (*str++)
+		len++;
+	return (len);
+}
 
 static void	ft_putchar(char str, int *len)
 {
@@ -40,6 +50,21 @@ static void	ft_putnbr_fd(int n, int fd, int *len)
 		ft_putnbr_fd((num % 10), fd, len);
 	}
 }
+static void ft_putnbr_base_fd(unsigned long n, char *base, int fd, int *len)
+{
+	unsigned long	num;
+	unsigned long	base_len;
+
+	base_len = ft_strlen(base);
+	num = n;
+	if (num < base_len)
+		ft_putchar(base[num], len);
+	else
+	{
+		ft_putnbr_base_fd((num / base_len), base, fd, len);
+		ft_putnbr_base_fd((num % base_len), base, fd, len);
+	}
+}
 
 void	ft_putstr(char const *str, int *len)
 {
@@ -47,10 +72,10 @@ void	ft_putstr(char const *str, int *len)
 	{
 		*len += write(1, str, 1);
 		str++;
-	}	
+	}
 }
 
-void ft_type(char const *str, int *len, va_list ap)
+void	ft_type(char const *str, int *len, va_list ap)
 {
 	if (*str == 's')
 		ft_putstr(va_arg(ap, char *), len);
@@ -61,6 +86,8 @@ void ft_type(char const *str, int *len, va_list ap)
 	else if (*str == 'd')
 		ft_putnbr_fd(va_arg(ap, int), 1, len);
 	else if (*str == 'i')
+		ft_putnbr_fd(va_arg(ap, int), 1, len);
+	else if (*str == 'u')
 		ft_putnbr_fd(va_arg(ap, int), 1, len);
 	else if (*str == '%')
 		ft_putchar(*str, len);
